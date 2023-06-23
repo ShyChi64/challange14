c4595170a30ecec9cdead06d74052ebdbe3d2cac
Kelsi <thacker.kl.01@gmail.com>
2023-06-22 20:49:31 -0700

    trying to deploy pt3

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3306;
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -34,7 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(require('./controllers/'));
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(process.env.PORT || 3001, function(){
+    app.listen(process.env.PORT || 3306, function(){
         console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
       });
 });
